f1f8181c026d56ad9fb68fa14e3f694833466b2c
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-10-20 15:07:10 +0330

    Added comments.

--- src/TaskMonitoring.Application/ThreadTaskMonitor.cs
+++ src/TaskMonitoring.Application/ThreadTaskMonitor.cs
@@ -6,10 +6,19 @@ namespace TaskMonitoring.Application;
 
 public class ThreadPoolMonitor : IThreadPoolMonitor
 {
+    /// <summary>
+    /// Retrieves the total number of active threads in the thread pool.
+    /// </summary>
+    /// <returns>The current active thread count in the thread pool.</returns>
     public int GetActiveThreadCount()
     {
         return ThreadPool.ThreadCount;
     }
+
+    /// <summary>
+    /// Retrieves the current status of the thread pool, including the number of available worker and IO threads.
+    /// </summary>
+    /// <returns>A ThreadPoolStatus object containing the number of available worker and IO threads.</returns>
     public ThreadPoolStatus GetThreadPoolStatus()
     {
         ThreadPool.GetAvailableThreads(out int workerThreads, out int ioThreads);
