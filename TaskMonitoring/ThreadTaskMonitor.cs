7823f8974112668dec9798073be00a5ea5a9ee77
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-10-20 14:31:55 +0330

    Initialized project

---
+++ TaskMonitoring/ThreadTaskMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitoring;
+
+public class ThreadTaskMonitor
+{
+    public int GetActiveThreadCount()
+    {
+        return ThreadPool.ThreadCount;
+    }
+ 
+ 
+}
