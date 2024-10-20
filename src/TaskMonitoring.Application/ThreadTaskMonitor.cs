1616cfb9a58f357d2364f7994d1d9db1e6f39139
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-10-20 16:08:08 +0330

    Added a method to set max and min of worker threads count.

--- src/TaskMonitoring.Application/ThreadTaskMonitor.cs
+++ src/TaskMonitoring.Application/ThreadTaskMonitor.cs
@@ -24,4 +24,22 @@ public class ThreadPoolMonitor : IThreadPoolMonitor
         ThreadPool.GetAvailableThreads(out int workerThreads, out int ioThreads);
         return new ThreadPoolStatus { WorkerThreadsAvailable = workerThreads, IoThreadsAvailable = ioThreads };
     }
+
+    /// <summary>
+    /// Sets the minimum number of threads that the thread pool maintains for worker and IO threads.
+    /// </summary>
+    /// <param name="threadPoolStatus">The minimum number of worker threads and the minimum number of IO threads.</param>
+    public void SetMinThreads(ThreadPoolStatus threadPoolStatus)
+    {
+        ThreadPool.SetMinThreads(threadPoolStatus.WorkerThreadsAvailable, threadPoolStatus.IoThreadsAvailable);
+    }
+
+    /// <summary>
+    /// Sets the maximum number of threads that the thread pool can maintain for worker and IO threads.
+    /// </summary>
+    /// <param name="workerThreads">The maximum number of worker threads and the maximum number of IO threads.</param>
+    public void SetMaxThreads(ThreadPoolStatus threadPoolStatus)
+    {
+        ThreadPool.SetMaxThreads(threadPoolStatus.WorkerThreadsAvailable, threadPoolStatus.IoThreadsAvailable);
+    }
 }
