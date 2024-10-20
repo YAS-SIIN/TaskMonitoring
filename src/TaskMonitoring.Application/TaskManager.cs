82c33ce8d35dfd3680422f1d39596f05b3bf19f7
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-10-20 15:09:02 +0330

    Added method to get some status from tasks.

--- src/TaskMonitoring.Application/TaskManager.cs
+++ src/TaskMonitoring.Application/TaskManager.cs
@@ -8,4 +8,22 @@ public class TaskManager : ITaskManager
 {
 
     private readonly ConcurrentDictionary<int, TaskInfo> _tasks = new();
+     
+    /// <summary>
+    /// Retrieves the count of pending tasks, which are either waiting to run or currently running.
+    /// </summary>
+    /// <returns>The count of pending tasks.</returns>
+    public int GetPendingTaskCount()
+    {
+        return _tasks.Values.Count(t => t.Status == TaskStatus.WaitingToRun || t.Status == TaskStatus.Running);
+    }
+
+    /// <summary>
+    /// Returns a list of details about all tracked tasks, including their ID, status, and duration.
+    /// </summary>
+    /// <returns>A list of TaskInfo objects representing the tracked tasks.</returns>
+    public List<TaskInfo> GetTaskDetails()
+    {
+        return _tasks.Values.ToList();
+    }
 }
