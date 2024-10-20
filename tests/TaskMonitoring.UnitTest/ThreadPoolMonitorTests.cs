f7e8e84e8146bb0c1b2584ccbd09396f2887db6b
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2024-10-20 15:23:32 +0330

    Codded test for GetThreadPoolStatus method.

--- tests/TaskMonitoring.UnitTest/ThreadPoolMonitorTests.cs
+++ tests/TaskMonitoring.UnitTest/ThreadPoolMonitorTests.cs
@@ -1,16 +1,32 @@
 using TaskMonitoring.Application;
+using TaskMonitoring.Application.Models;
 
 namespace TaskMonitoring.UnitTest;
 
 public class ThreadPoolMonitorTests
 {
+    private readonly ThreadPoolMonitor threadPoolMonitor;
+    public ThreadPoolMonitorTests()
+    {
+        threadPoolMonitor = new ThreadPoolMonitor();
+    }
+
     [Fact]
     public void GetActiveThreadCount_ShouldReturnValidCount()
     {
-        var threadPoolMonitor = new ThreadPoolMonitor();
-
         var activeThreadCount = threadPoolMonitor.GetActiveThreadCount();
 
-        Assert.True(activeThreadCount >= 0); 
+        Assert.True(activeThreadCount >= 0);
     }
+
+    [Fact]
+    public void GetThreadPoolStatus_ShouldReturnCorrectThreadAvailability()
+    { 
+        ThreadPoolStatus status = threadPoolMonitor.GetThreadPoolStatus();
+         
+        Assert.NotNull(status);
+        Assert.True(status.WorkerThreadsAvailable >= 0);
+        Assert.True(status.IoThreadsAvailable >= 0);
+    }
+
 }
